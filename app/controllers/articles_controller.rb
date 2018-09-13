6d885bf2d1dba828f04f6f9db398ceb454cf48e6
damianprog <damian.pukszta1@gmail.com>
2018-09-13 11:28:28 +0200

    Add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
-  before_action :set_article,only: [:edit,:update,:show,:destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
-      flash[:success] = "Article was successfully created"
+      flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:success] = "Article was successfully updated"
+      flash[:success] = 'Article was successfully updated'
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -37,14 +37,16 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:danger] = "Article was successfully deleted"
+    flash[:danger] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
   private
+
   def set_article
     @article = Article.find(params[:id])
   end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
